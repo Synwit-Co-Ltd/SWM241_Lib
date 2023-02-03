23381add8efa7e7cd2dc96ee81123ef3e21c66ec
yanmowudi <whsj215294062@outlook.com>
2023-02-03 15:16:02 +0800

    modify TIMR pin

--- TIMR/OutputCompare/APP/main.c
+++ TIMR/OutputCompare/APP/main.c
@@ -6,7 +6,6 @@ int main(void)
 	SystemInit();
 	
 	PORT_Init(PORTB, PIN2,  PORTB_PIN2_TIMR2_OUT, 0);
-	PORT_Init(PORTB, PIN14, PORTB_PIN14_TIMR2_OUT, 0);
 	
 	TIMR_Init(TIMR2, TIMR_MODE_TIMER, 1, 10000, 0);	//输出波形时TIMR只有16位，频率为24MHz/10000 = 2.4KHz
 	
