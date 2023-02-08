7285105d0fc8860e39c42309b119a8adee732187
yanmowudi <whsj215294062@outlook.com>
2023-02-08 17:33:28 +0800

    add I2C and SPI samples

--- SWM241_StdPeriph_Driver/CSL/SWM241_StdPeriph_Driver/SWM241_i2c.c
+++ SWM241_StdPeriph_Driver/CSL/SWM241_StdPeriph_Driver/SWM241_i2c.c
@@ -49,9 +49,9 @@ void I2C_Init(I2C_TypeDef * I2Cx, I2C_InitStructure * initStruct)
 	{
 		I2Cx->CR |= (1 << I2C_CR_MASTER_Pos);
 		
-		I2Cx->CLK = ((SystemCoreClock/1000000/3*2)  << I2C_CLK_SCLL_Pos) |
-					((SystemCoreClock/1000000/3*1)  << I2C_CLK_SCLH_Pos) |
-					((1000000 / initStruct->MstClk) << I2C_CLK_DIV_Pos);
+		I2Cx->CLK = ((SystemCoreClock / 1000000 / 3 * 2 - 1)  << I2C_CLK_SCLL_Pos) |
+					((SystemCoreClock / 1000000 / 3 * 1 - 1)  << I2C_CLK_SCLH_Pos) |
+					((1000000 / initStruct->MstClk - 1) << I2C_CLK_DIV_Pos);
 		
 		I2Cx->IF = 0xFFFFFFFF;
 		I2Cx->IE = (initStruct->TXEmptyIEn << I2C_IE_TXE_Pos) |
@@ -158,16 +158,31 @@ void I2C_Close(I2C_TypeDef * I2Cx)
 * 功能说明:	产生起始信号并发送设备地址
 * 输    入: I2C_TypeDef * I2Cx		指定要被设置的I2C，有效值包括I2C0、I2C1
 *			uint8_t addr			设备地址
+*			uint8_t wait			是否等待发送完成，1 等待发送完成   0 不等待，立即返回
 * 输    出: uint8_t					1 接收到ACK   0 接收到NACK
-* 注意事项: 无
+* 注意事项: 若选择不等待，立即返回，则返回值无意义；后续可通过 I2C_StartDone() 查询是否完成，完成后调用 I2C_IsAck() 查询接收到ACK还是NACK
 ******************************************************************************************************************************************/
-uint8_t I2C_Start(I2C_TypeDef * I2Cx, uint8_t addr)
+uint8_t I2C_Start(I2C_TypeDef * I2Cx, uint8_t addr, uint8_t wait)
 {
 	I2Cx->TXDATA = addr;
 	I2Cx->MCR = (1 << I2C_MCR_STA_Pos) |		
 				(1 << I2C_MCR_WR_Pos);				//发送起始位和从机地址
+	
+	if(wait == 0)
+		return 0;
+	
 	while(I2Cx->MCR & I2C_MCR_WR_Msk) __NOP();		//等待发送完成
 	
+	return (I2Cx->TR & I2C_TR_RXACK_Msk) ? 0 : 1;
+}
+
+uint8_t I2C_StartDone(I2C_TypeDef * I2Cx)
+{
+	return (I2Cx->MCR & I2C_MCR_WR_Msk) ? 0 : 1;
+}
+
+uint8_t I2C_IsAck(I2C_TypeDef * I2Cx)
+{
 	return (I2Cx->TR & I2C_TR_RXACK_Msk) ? 0 : 1;
 }
 
@@ -175,13 +190,23 @@ uint8_t I2C_Start(I2C_TypeDef * I2Cx, uint8_t addr)
 * 函数名称:	I2C_Stop()
 * 功能说明:	产生停止信号
 * 输    入: I2C_TypeDef * I2Cx		指定要被设置的I2C，有效值包括I2C0、I2C1
+*			uint8_t wait			是否等待发送完成，1 等待发送完成   0 不等待，立即返回
 * 输    出: 无
-* 注意事项: 无
+* 注意事项: 若选择不等待，立即返回；后续可通过 I2C_StopDone() 查询是否完成
 ******************************************************************************************************************************************/
-void I2C_Stop(I2C_TypeDef * I2Cx)
+void I2C_Stop(I2C_TypeDef * I2Cx, uint8_t wait)
 {
 	I2Cx->MCR = (1 << I2C_MCR_STO_Pos);
+	
+	if(wait == 0)
+		return;
+	
 	while(I2Cx->MCR & I2C_MCR_STO_Msk) __NOP();		//等待发送完成
+}
+
+uint8_t I2C_StopDone(I2C_TypeDef * I2Cx)
+{
+	return (I2Cx->MCR & I2C_MCR_STO_Msk) ? 0 : 1;
 }
 
 /****************************************************************************************************************************************** 
@@ -189,16 +214,26 @@ void I2C_Stop(I2C_TypeDef * I2Cx)
 * 功能说明:	写入一个数据
 * 输    入: I2C_TypeDef * I2Cx		指定要被设置的I2C，有效值包括I2C0、I2C1
 *			uint8_t data			要写的数据
+*			uint8_t wait			是否等待发送完成，1 等待发送完成   0 不等待，立即返回
 * 输    出: uint8_t					1 接收到ACK   0 接收到NACK
-* 注意事项: 无
+* 注意事项: 若选择不等待，立即返回，则返回值无意义；后续可通过 I2C_WriteDone() 查询是否完成，完成后调用 I2C_IsAck() 查询接收到ACK还是NACK
 ******************************************************************************************************************************************/
-uint8_t I2C_Write(I2C_TypeDef * I2Cx, uint8_t data)
+uint8_t I2C_Write(I2C_TypeDef * I2Cx, uint8_t data, uint8_t wait)
 {	
 	I2Cx->TXDATA = data;
 	I2Cx->MCR = (1 << I2C_MCR_WR_Pos);
+	
+	if(wait == 0)
+		return 0;
+	
 	while(I2Cx->MCR & I2C_MCR_WR_Msk) __NOP();		//等待发送完成
 	
 	return (I2Cx->TR & I2C_TR_RXACK_Msk) ? 0 : 1;
+}
+
+uint8_t I2C_WriteDone(I2C_TypeDef * I2Cx)
+{
+	return (I2Cx->MCR & I2C_MCR_WR_Msk) ? 0 : 1;
 }
 
 /****************************************************************************************************************************************** 
@@ -206,15 +241,81 @@ uint8_t I2C_Write(I2C_TypeDef * I2Cx, uint8_t data)
 * 功能说明:	读取一个数据
 * 输    入: I2C_TypeDef * I2Cx		指定要被设置的I2C，有效值包括I2C0、I2C1
 *			uint8_t ack				1 发送ACK   0 发送NACK
+*			uint8_t wait			是否等待发送完成，1 等待发送完成   0 不等待，立即返回
 * 输    出: uint8_t					读取到的数据
-* 注意事项: 无
+* 注意事项: 若选择不等待，立即返回，则返回值无意义；后续可通过 I2C_ReadDone() 查询是否完成，完成后通过 I2Cx->RXDATA 获取读取到的数据
 ******************************************************************************************************************************************/
-uint8_t I2C_Read(I2C_TypeDef * I2Cx, uint8_t ack)
+uint8_t I2C_Read(I2C_TypeDef * I2Cx, uint8_t ack, uint8_t wait)
 {
 	I2Cx->TR = ((ack ? 0 : 1) << I2C_TR_TXACK_Pos);
 	
 	I2Cx->MCR = (1 << I2C_MCR_RD_Pos);
+	
+	if(wait == 0)
+		return 0;
+	
 	while(I2Cx->MCR & I2C_MCR_RD_Msk) __NOP();		//等待接收完成
     
     return I2Cx->RXDATA;
+}
+
+uint8_t I2C_ReadDone(I2C_TypeDef * I2Cx)
+{
+	return (I2Cx->MCR & I2C_MCR_RD_Msk) ? 0 : 1;
+}
+
+/****************************************************************************************************************************************** 
+* 函数名称:	I2C_INTEn()
+* 功能说明:	中断使能
+* 输    入: I2C_TypeDef * I2Cx		指定要被设置的I2C，有效值包括I2C0、I2C1
+* 			uint32_t it				interrupt type，有效值I2C_IT_TX_EMPTY、I2C_IT_RX_NOT_EMPTY、I2C_IT_RX_OVF、I2C_IT_TX_DONE、I2C_IT_RX_DONE、
+*									I2C_IT_SLV_DET_STA、I2C_IT_SLV_DET_STP、I2C_IT_ARB_LOST、I2C_IT_SCL_LOW_TO 及其“或”
+* 输    出: 无
+* 注意事项: 无
+******************************************************************************************************************************************/
+void I2C_INTEn(I2C_TypeDef * I2Cx, uint32_t it)
+{
+	I2Cx->IE |= it;
+}
+
+/****************************************************************************************************************************************** 
+* 函数名称:	I2C_INTDis()
+* 功能说明:	中断禁止
+* 输    入: I2C_TypeDef * I2Cx		指定要被设置的I2C，有效值包括I2C0、I2C1
+* 			uint32_t it				interrupt type，有效值I2C_IT_TX_EMPTY、I2C_IT_RX_NOT_EMPTY、I2C_IT_RX_OVF、I2C_IT_TX_DONE、I2C_IT_RX_DONE、
+*									I2C_IT_SLV_DET_STA、I2C_IT_SLV_DET_STP、I2C_IT_ARB_LOST、I2C_IT_SCL_LOW_TO 及其“或”
+* 输    出: 无
+* 注意事项: 无
+******************************************************************************************************************************************/
+void I2C_INTDis(I2C_TypeDef * I2Cx, uint32_t it)
+{
+	I2Cx->IE &= ~it;
+}
+
+/****************************************************************************************************************************************** 
+* 函数名称:	I2C_INTClr()
+* 功能说明:	中断标志清除
+* 输    入: I2C_TypeDef * I2Cx		指定要被设置的I2C，有效值包括I2C0、I2C1
+* 			uint32_t it				interrupt type，有效值I2C_IT_TX_EMPTY、I2C_IT_RX_NOT_EMPTY、I2C_IT_RX_OVF、I2C_IT_TX_DONE、I2C_IT_RX_DONE、
+*									I2C_IT_SLV_DET_STA、I2C_IT_SLV_DET_STP、I2C_IT_ARB_LOST、I2C_IT_SCL_LOW_TO 及其“或”
+* 输    出: 无
+* 注意事项: 无
+******************************************************************************************************************************************/
+void I2C_INTClr(I2C_TypeDef * I2Cx, uint32_t it)
+{
+	I2Cx->IF = it;
+}
+
+/****************************************************************************************************************************************** 
+* 函数名称:	I2C_INTStat()
+* 功能说明:	中断状态查询
+* 输    入: I2C_TypeDef * I2Cx		指定要被设置的I2C，有效值包括I2C0、I2C1
+* 			uint32_t it				interrupt type，有效值I2C_IT_TX_EMPTY、I2C_IT_RX_NOT_EMPTY、I2C_IT_RX_OVF、I2C_IT_TX_DONE、I2C_IT_RX_DONE、
+*									I2C_IT_SLV_DET_STA、I2C_IT_SLV_DET_STP、I2C_IT_ARB_LOST、I2C_IT_SCL_LOW_TO 及其“或”
+* 输    出: uint32_t				1 中断发生    0 中断未发生
+* 注意事项: 无
+******************************************************************************************************************************************/
+uint32_t I2C_INTStat(I2C_TypeDef * I2Cx, uint32_t it)
+{
+	return (I2Cx->IF & it) ? 1 : 0;
 }
--- SWM241_StdPeriph_Driver/CSL/SWM241_StdPeriph_Driver/SWM241_i2c.h
+++ SWM241_StdPeriph_Driver/CSL/SWM241_StdPeriph_Driver/SWM241_i2c.h
@@ -17,15 +17,37 @@ typedef struct {
 } I2C_InitStructure;
 
 
+/* Interrupt Type */
+#define I2C_IT_TX_EMPTY		(1 <<  0)	//TX FIFO Empty
+#define I2C_IT_RX_NOT_EMPTY	(1 <<  1)	//RX FIFO Not Empty
+#define I2C_IT_RX_OVF		(1 <<  2)	//RX FIFO Overflow
+#define I2C_IT_TX_DONE		(1 <<  3)	//发送完成（接收到ACK）
+#define I2C_IT_RX_DONE		(1 <<  4)	//接收完成（发送出ACK）
+#define I2C_IT_SLV_DET_STA	(1 <<  8)	//从机检测到起始位
+#define I2C_IT_SLV_DET_STP	(1 <<  9)	//从机检测到停止位
+#define I2C_IT_ARB_LOST		(1 << 16)	//主机Arbitration lost
+#define I2C_IT_SCL_LOW_TO	(1 << 17)	//主机SCL Low Timeout
+
+
 void I2C_Init(I2C_TypeDef * I2Cx, I2C_InitStructure * initStruct);
 
 void I2C_Open(I2C_TypeDef * I2Cx);
 void I2C_Close(I2C_TypeDef * I2Cx);
 
-uint8_t I2C_Start(I2C_TypeDef * I2Cx, uint8_t addr);
-void I2C_Stop(I2C_TypeDef * I2Cx);
-uint8_t I2C_Write(I2C_TypeDef * I2Cx, uint8_t data);
-uint8_t I2C_Read(I2C_TypeDef * I2Cx, uint8_t ack);
+uint8_t I2C_Start(I2C_TypeDef * I2Cx, uint8_t addr, uint8_t wait);
+void I2C_Stop(I2C_TypeDef * I2Cx, uint8_t wait);
+uint8_t I2C_Write(I2C_TypeDef * I2Cx, uint8_t data, uint8_t wait);
+uint8_t I2C_Read(I2C_TypeDef * I2Cx, uint8_t ack, uint8_t wait);
 
+uint8_t I2C_StartDone(I2C_TypeDef * I2Cx);
+uint8_t I2C_StopDone(I2C_TypeDef * I2Cx);
+uint8_t I2C_WriteDone(I2C_TypeDef * I2Cx);
+uint8_t I2C_ReadDone(I2C_TypeDef * I2Cx);
+uint8_t I2C_IsAck(I2C_TypeDef * I2Cx);
+
+void I2C_INTEn(I2C_TypeDef * I2Cx, uint32_t it);		//中断使能
+void I2C_INTDis(I2C_TypeDef * I2Cx, uint32_t it);		//中断禁止
+void I2C_INTClr(I2C_TypeDef * I2Cx, uint32_t it);		//中断标志清除
+uint32_t I2C_INTStat(I2C_TypeDef * I2Cx, uint32_t it);	//中断状态查询
 
 #endif //__SWM241_I2C_H__
